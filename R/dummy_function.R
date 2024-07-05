6fb0af39765cb080826ad3965543d384761ce8fa
paulachristen <pc16@ic.ac.uk>
2024-07-05 16:57:46 +0300

    moved all files

---
+++ R/dummy_function.R
@@ -0,0 +1,12 @@
+#' Add Two Numbers
+#'
+#' This function takes two numeric inputs and returns their sum.
+#'
+#' @param x A numeric value.
+#' @param y A numeric value.
+#' @return The sum of x and y.
+#' @examples
+#' add_two_numbers(3, 5)  # Returns 8
+add_two_numbers <- function(x, y) {
+  x + y
+}
