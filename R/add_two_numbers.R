b4f2bc91492289939d73f0765dac701d9577f2f3
paulachristen <pc16@ic.ac.uk>
2024-07-08 10:51:54 +0300

    removed all frills

--- R/add_two_numbers.R
+++ R/add_two_numbers.R
@@ -1,10 +1,3 @@
-#' Add Two Numbers
-#'
-#' This function takes two numeric inputs and returns their sum.
-#'
-#' @param x A numeric value.
-#' @param y A numeric value.
-#' @return The sum of x and y.
 add_two_numbers <- function(x, y) {
   x + y
 }
