950586d810727a19f228e1021bda754ea6499924
paulachristen <pc16@ic.ac.uk>
2024-07-08 11:15:19 +0300

    manual changed - removed examples

--- R/add_two_numbers.R
+++ R/add_two_numbers.R
@@ -5,9 +5,6 @@
 #' @param x A numeric value.
 #' @param y A numeric value.
 #' @return The sum of x and y.
-#' @examples
-#' add_two_numbers(3, 5)  # Returns 8
-#'
 add_two_numbers <- function(x, y) {
   x + y
 }
