0c43b57b619d2e9375488abdc686348ca28cacb3
paulachristen <pc16@ic.ac.uk>
2024-08-07 17:19:07 +0300

    formating

--- R/prep_forecast.R
+++ R/prep_forecast.R
@@ -52,13 +52,11 @@ prep_forecast_data <- function(data, forecast_type, ...) {
 #' @return A data frame with cleaned numeric columns.
 clean_numeric_columns <- function(data, numeric_columns) {
   data %>%
-    dplyr::mutate(across(any_of(numeric_columns), ~
-                           as.numeric(gsub("\\(|\\)", "", .x)))) %>%
-    dplyr::mutate(across(any_of(numeric_columns), ~
-                           ifelse(
-                             grepl("^n\\.?a\\.?n\\.?$", .x,
-                                   ignore.case = TRUE), NA, .x
-                           )))
+    dplyr::mutate(across(any_of(numeric_columns), ~ as.numeric(gsub("\\(|\\)", "", .x))),
+                  dplyr::mutate(across(any_of(numeric_columns), ~ ifelse(
+                    grepl("^n\\.?a\\.?n\\.?$", .x,
+                          ignore.case = TRUE), NA, .x
+                  ))))
 }
 
 #' Prepare quantile forecast data
